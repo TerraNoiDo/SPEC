4681ca5a69742e973d574f4ca73624af37a70049
TerraNoiDo <spec_3@ukr.net>
2024-06-19 18:24:42 +0300

    First project

--- Lession_3_1.py
+++ Lession_3_1.py
@@ -19,3 +19,24 @@ print(str(list_value_3))
 
 list_value_4 = list_value_4[-1:] + list_value_4[:-1]
 print(str(list_value_4))
+
+
+######################## Ще один варіант, але не зміг перевірку list_value_3 ################################
+
+
+# list_value_1 = [12, 3, 4, 10]
+# list_value_2 = [1]
+# list_value_3 = []
+# list_value_4 = [12, 3, 4, 10, 8]
+#
+# list_value_1.insert(0, list_value_1.pop())
+# print(str(list_value_1))
+#
+# list_value_2.insert(0, list_value_2.pop())
+# print(str(list_value_2))
+#
+# list_value_3.insert(0, list_value_3.pop())
+# print(str(list_value_2))
+#
+# list_value_4.insert(0, list_value_4.pop())
+# print(str(list_value_4))
--- Testing.py
+++ Testing.py
@@ -3,14 +3,14 @@ list_value_2 = [1]
 list_value_3 = []
 list_value_4 = [12, 3, 4, 10, 8]
 
-list_value_1 = list_value_1[-1:] + list_value_1[:-1]
+list_value_1.insert(0, list_value_1.pop())
 print(str(list_value_1))
 
-list_value_2 = list_value_2[-1:] + list_value_2[:-1]
+list_value_2.insert(0, list_value_2.pop())
 print(str(list_value_2))
 
-list_value_3 = list_value_3[-1:] + list_value_3[:-1]
-print(str(list_value_3))
+list_value_3.insert(0, list_value_3.pop())
+print(str(list_value_2))
 
-list_value_4 = list_value_4[-1:] + list_value_4[:-1]
+list_value_4.insert(0, list_value_4.pop())
 print(str(list_value_4))
